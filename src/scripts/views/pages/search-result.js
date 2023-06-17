ddb1c48064560d3f2309a37ba140259496bb0a7e
nadyadf <nadyadf3@gmail.com>
2023-06-17 16:02:31 +0700

    cleaning duplicate code

--- src/scripts/views/pages/search-result.js
+++ src/scripts/views/pages/search-result.js
@@ -18,7 +18,7 @@ const SearchResult = {
           </div>
         </div>
       </section>
-      <h2 class="title-section">Hasil Pencarian: <span id="search-type"></span></h2>
+      <h2 class="title-section">Hasil Pencarian: <span id="search-key"></span></h2>
       <p class="result-total"></p>
       ${CreateSortFeatureTemplate()}
       <section class="result"></section>
@@ -26,14 +26,28 @@ const SearchResult = {
   },
 
   async afterRender() {
+    // Initialization
     DrawerInitiator.init({
       className: 'show',
       button: document.querySelector('.btn-sort'),
       drawer: document.querySelector('.sort-mini-modal'),
       content: document.querySelector('#main'),
     });
-
+    const categoriesContainer = document.querySelector('.categories-container');
     const resultContainer = document.querySelector('.result');
+    const searchInput = document.querySelector('#keyword-input');
+    const btnSearch = document.querySelector('.bi-search');
+    const searchKeyTitle = document.querySelector('#search-key');
+    const resultTotal = document.querySelector('.result-total');
+    const sortItemElements = document.querySelectorAll('.sort-item');
+
+    let searchResults = '';
+
+    // Show Categories
+    const categories = await StockTrackerResource.showAllCategories();
+    categories.forEach((category) => {
+      categoriesContainer.innerHTML += CreateCategoryLabel(category);
+    });
 
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const { keyword, resource, categoryId } = url;
@@ -41,57 +55,51 @@ const SearchResult = {
     if (keyword) {
       const keywordWithoutEncoding = keyword.replace('%20', ' ');
 
-      document.querySelector('#keyword-input').value = keywordWithoutEncoding;
-      document.querySelector('#search-type').innerText = keywordWithoutEncoding;
+      searchInput.value = keywordWithoutEncoding;
+      searchKeyTitle.innerText = keywordWithoutEncoding;
 
       // Fetch product from database by query params
-      const searchResults = await StockTrackerResource.searchProductResult(keyword);
+      searchResults = await StockTrackerResource.searchProductResult(keyword);
 
-      document.querySelector('.result-total').innerText = `Total: ${searchResults.length}`;
+      resultTotal.innerText = `Total: ${searchResults.length}`;
       searchResults.forEach((product) => {
         resultContainer.innerHTML += CreateProductItem(product);
       });
     } else {
       // Fetch product from database by category
-      const searchResults = await StockTrackerResource.getProductsByCategory(categoryId);
+      searchResults = await StockTrackerResource.getProductsByCategory(categoryId);
       const firstProduct = searchResults[0];
       const categoryName = firstProduct ? firstProduct.category_name : '';
 
-      document.querySelector('#search-type').innerText = categoryName ? `kategori ${categoryName}` : '';
+      searchKeyTitle.innerText = categoryName ? `kategori ${categoryName}` : '';
 
-      document.querySelector('.result-total').innerText = `Total: ${searchResults.length}`;
+      resultTotal.innerText = `Total: ${searchResults.length}`;
       searchResults.forEach((product) => {
         resultContainer.innerHTML += CreateProductItem(product);
       });
     }
 
-    // Show Categories
-    const categoriesContainer = document.querySelector('.categories-container');
-    const categories = await StockTrackerResource.showAllCategories();
-    categories.forEach((category) => {
-      categoriesContainer.innerHTML += CreateCategoryLabel(category);
-    });
-
-    document.querySelector('.bi-search').addEventListener('click', (e) => {
+    btnSearch.addEventListener('click', (e) => {
       e.preventDefault();
-      const keywordInput = document.querySelector('.form-control').value;
-      window.location.replace(`/#/${resource}?keyword=${keywordInput}`);
+      const keywordInputValue = searchInput.value;
+      window.location.replace(`/#/${resource}?keyword=${keywordInputValue}`);
     });
 
-    document.querySelector('.sort-item.price').addEventListener('click', async (e) => {
+    // Sort by price button
+    sortItemElements[0].addEventListener('click', async (e) => {
       e.preventDefault();
       resultContainer.innerHTML = '';
 
       if (keyword) {
-        const searchResults = await StockTrackerResource.sortProductsByPrice('keyword', keyword);
-        document.querySelector('.result-total').innerText = `Total: ${searchResults.length}`;
+        searchResults = await StockTrackerResource.sortProductsByPrice('keyword', keyword);
+        resultTotal.innerText = `Total: ${searchResults.length}`;
 
         searchResults.forEach((product) => {
           resultContainer.innerHTML += CreateProductItem(product);
         });
       } else {
-        const searchResults = await StockTrackerResource.sortProductsByPrice('categoryId', categoryId);
-        document.querySelector('.result-total').innerText = `Total: ${searchResults.length}`;
+        searchResults = await StockTrackerResource.sortProductsByPrice('categoryId', categoryId);
+        resultTotal.innerText = `Total: ${searchResults.length}`;
 
         searchResults.forEach((product) => {
           resultContainer.innerHTML += CreateProductItem(product);
