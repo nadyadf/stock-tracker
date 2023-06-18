afdc86e5b0446f0cd756abd3aae348954bc939d9
nadyadf <nadyadf3@gmail.com>
2023-06-19 00:32:20 +0700

    edit create market item

--- src/scripts/views/templates/create-market-item .js
+++ src/scripts/views/templates/create-market-item .js
@@ -1,9 +1,9 @@
 const CreateMarketItem = (market) => `
   <div class="card market">
     <div class="card-body">
-      <a class="card-title" href="/#/detail_market?market_id=${market.market_id}">${market.name}</a>
+      <a class="card-title" href="/#/detail_market?market_id=${market.market_id}">${market.market_name}</a>
       <p class="card-subtitle">${market.address}</p>
-      <p class="card-text">${market.description}</p>
+      <p class="card-text">${market.market_type}</p>
       <p class="card-text">${market.rating}</p>
     </div>
   </div>
